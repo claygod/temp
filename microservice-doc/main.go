e969029dc3c1a1877fc95a66b46880ed37617c23
Eduard <claygod@yandex.ru>
2016-12-29 12:00:13 +0300

    Add files via upload

--- microservice-doc/main.go
+++ microservice-doc/main.go
@@ -1,6 +1,6 @@
 //This library provides a simple framework of microservice, which includes
 //a configurator, a logger, metrics, and of course the handler
-package main
+package microservice
 
 // Microservice
 // Main
