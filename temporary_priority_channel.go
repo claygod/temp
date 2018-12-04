67ca0a364c092f78eb7f1d0afd3ec70ea60fb3df
Eduard <claygod@yandex.ru>
2018-12-04 11:39:44 +0300

    Update temporary_priority_channel.go

--- temporary_priority_channel.go
+++ temporary_priority_channel.go
@@ -4,3 +4,13 @@
 	fmt.Println("=++++=", efacez.dataqsiz)
 	fmt.Println("=++++=", efacez.qcount)
 	fmt.Println("=++++=", efacez.dataqsiz)
+
+
+func chanbufr(c *hchan, i uint) unsafe.Pointer {
+	for u := i; u > 0; u-- {
+		p1 := (*unsafe.Pointer)(unsafe.Pointer(uintptr(c.buf) + uintptr(u-1)*uintptr(c.elemsize)))
+		p2 := (*unsafe.Pointer)(unsafe.Pointer(uintptr(c.buf) + uintptr(u)*uintptr(c.elemsize)))
+		*p2 = *p1
+	}
+	return c.buf
+}
